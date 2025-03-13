9ee86c3072b53a6a3d9748c7dcd1db3741ed3cde
PrakharSoni1998 <prakharsoni1998@gmail.com>
2025-03-13 19:11:03 +0530

    query and param in routes

--- src/app.js
+++ src/app.js
@@ -3,25 +3,12 @@ const express = require('express')
 const app = express()
 
 
-app.get("/user",(req,res)=>{
+app.get('/users/:userId/:name',(req,res)=>{
+    console.log(req.params);
+    
     res.send({name:"tetst"})
 })
 
-app.post("/user",(req,res)=>{
-    res.send("User saved successfully.")
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("User updated successfully")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("User deleted successfully")
-})
-
-app.use('/test',(req,res)=>{
-    res.send('Test Page')
-})
 
 app.listen(3000,()=>{
 console.log("Server is running on port 3000")
