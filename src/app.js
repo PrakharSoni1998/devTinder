b3f7f97c388cd94049352c412858cbb159cd3db4
PrakharSoni1998 <prakharsoni1998@gmail.com>
2025-03-13 19:02:01 +0530

    get,post ,patch and delete method practice

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const app = express()
 
+
+app.get("/user",(req,res)=>{
+    res.send({name:"tetst"})
+})
+
+app.post("/user",(req,res)=>{
+    res.send("User saved successfully.")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("User updated successfully")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User deleted successfully")
+})
+
 app.use('/test',(req,res)=>{
-    res.send('test')
+    res.send('Test Page')
 })
 
 app.listen(3000,()=>{
